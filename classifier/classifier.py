480f72649da841ff4bf2b278813d94cdf39d8c47
周雪 <zhouxue@quancheng-ec.com>
2017-03-04 11:52:15 +0800

    修改贝叶斯公式用计算的bug

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,3 +1,17 @@
+
+import re
+import math
+
+def getwords(doc):
+    splitter=re.compile('\\W*')
+    print doc
+    # Split the words by non-alpha characters
+    words=[s.lower() for s in splitter.split(doc) 
+           if len(s)>2 and len(s)<20]
+
+    # Return the unique set of words only
+    return dict([(w,1) for w in words])
+
 class classifier:
     def __init__(self, features, file=None):
         self.fc = {}
@@ -9,11 +23,11 @@ class classifier:
         self.fc[f].setdefault(cat,0)
         self.fc[f][cat] += 1
     def incc(self, cat):
-        self.cc.setdefault(cat,{})
+        self.cc.setdefault(cat,0)
         self.cc[cat] += 1
         
-    def fcount(self, feature, cat):
-        if feature in self.fc and cat in self.fc[f]:
+    def fcount(self, f, cat):
+        if f in self.fc and cat in self.fc[f]:
             return float(self.fc[f][cat])
         return 0.0
     
@@ -30,22 +44,46 @@ class classifier:
     def train(self, item, cat):
         feature = self.features(item); #feature is what
         
-        for f in featur:
-            self.incfc(self, f, cat)
+        for f in feature:
+            self.incfc(f, cat)
         
         self.incc(cat)
    
     #calculate the properbility of P(A|B)     
     def fprob(self, f, cat):
-        pb = self.cc[cat]
-        pa = self.fc[f][cat]
+        if self.catcount(cat) == 0: return 0
         
-        prob = float(pa/pb) # float() ??
+        pb = self.catcount(cat)
+        pa = self.fcount(f, cat)
+        
+        prob = pa/pb # float() ??
+        return prob
         
     # reset prob by weight
     def weightedprob(self,f,cat,prf,weight=1.0,ap=0.5):
-        basicprob = fprob(f,cat)
+        basicprob = self.fprob(f,cat)
         totals=sum([self.fcount(f,c) for c in self.categories()])
         bp=((weight*ap)+(totals*basicprob))/(weight+totals)
         return bp
     
+    def sampletrain(self):
+        self.train('the quick brown fox jumps over the lazy dog', 'good')
+        self.train('make quick money in the online casino', 'bad')
+        self.train('Nobody owns the water', 'good')
+        self.train('the quick rabbit jumps fences', 'good')
+        self.train('buy pharmaceuticals now', 'bad')
+
+class naivebayes(classifier):
+    def docprob(self,item,cat):
+        features=self.features(item)
+        
+        p = 1
+        for f in features: p*=self.weightedprob(f,cat,self.fprob)
+        return p
+    
+    # get P(A|B)*P(A) A:Category B:Document
+    def prob(self, item, cat):
+        catprob = self.catcount(cat)/self.totalcount()
+        docprob = self.docprob(item, cat)
+        return docprob*catprob
+        
--- classifier/naivebayes.py
+++ classifier/naivebayes.py
@@ -1,3 +1,17 @@
-import sys,classifier
+import sys, classifier
 sys.path.append('./classifier.py')
-class naivebayes():
+
+
+c1=classifier.naivebayes(classifier.getwords)
+
+c1.sampletrain()
+
+
+#print(c1.fprob('quick','good'))
+
+#print(c1.weightedprob('money','good', c1.fprob))
+#c1.sampletrain()
+#print(c1.weightedprob('money','good', c1.fprob))
+
+print(c1.prob('quick rabbit','good'))
+print(c1.prob('quick rabbit','bad'))
