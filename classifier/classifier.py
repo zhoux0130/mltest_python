9ddf0c16cadc6f433cfdd13c13c0fecc43bda238
周雪 <zhouxue@quancheng-ec.com>
2017-03-05 12:43:23 +0800

    完成费舍尔聚类方法

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,4 +1,4 @@
-
+#from pysqlite2 import dbapi2 as sqlite
 import re
 import math
 
@@ -17,6 +17,11 @@ class classifier:
         self.fc = {}
         self.cc = {}
         self.features = features
+        
+    #def setdb(self,dbfile):
+        #self.con=sqlite.connect(dbfile)    
+        #self.con.execute('create table if not exists fc(feature,category,count)')
+        #self.con.execute('create table if not exists cc(category,count)')
           
     def incfc(self,f,cat):
         self.fc.setdefault(f,{})
@@ -60,7 +65,7 @@ class classifier:
         return prob
         
     # reset prob by weight
-    def weightedprob(self,f,cat,prf,weight=1.0,ap=0.5):
+    def weightedprob(self,f,cat,prf,weight=1,ap=0.5):
         basicprob = self.fprob(f,cat)
         totals=sum([self.fcount(f,c) for c in self.categories()])
         bp=((weight*ap)+(totals*basicprob))/(weight+totals)
@@ -74,6 +79,18 @@ class classifier:
         self.train('buy pharmaceuticals now', 'bad')
 
 class naivebayes(classifier):
+    
+    def __init__(self, getfeatures):
+        classifier.__init__(self, getfeatures)
+        self.thresholds={}
+        
+    def setthreholds(self,cat,t):
+        self.thresholds[cat] = t
+        
+    def getthreholds(self,cat):
+        if cat not in self.thresholds: return 1.0
+        return self.thresholds[cat]
+    
     def docprob(self,item,cat):
         features=self.features(item)
         
@@ -87,3 +104,87 @@ class naivebayes(classifier):
         docprob = self.docprob(item, cat)
         return docprob*catprob
         
+    def classify(self, item, default=None):
+        probs={}
+        #get prob max category
+        max = 0.0
+        for cat in self.categories():
+            probs[cat] = self.prob(item,cat)
+            if probs[cat]>max:
+                max = probs[cat]
+                best = cat
+                
+        for cat in probs:
+            if cat == best: continue
+            if probs[cat]*self.getthreholds(best)>probs[best]:return default
+            
+        return best
+    
+class fisherclassifier(classifier):
+    def cprob(self, f, cat):
+        clf = self.fprob(f,cat)
+        
+        if clf == 0: return 0
+        
+        freqsum = sum([self.fprob(f, c) for c in self.categories()])
+        
+        p = clf/freqsum
+        return p
+    
+    #def fisherprob(self, item, cat):
+        #p = 1
+        #features = self.features(item)
+        #for f in features:
+            #p*=(self.weightedprob(f, cat, self.cprob))
+        
+        #fscore=-2*math.log(p)
+        #return self.invchi2(fscore, len(features)*2)
+    
+    #def invchi2(self, chi, df):
+        #m = chi / 2.0
+        #sum = term = math.exp(-m)
+        #for i in range(1, df//2):
+            #term *= m/i
+            #sum += term
+        #return min(sum, 1.0)
+        
+    def fisherprob(self,item,cat):
+        # Multiply all the probabilities together
+        p=1
+        features=self.features(item)
+        for f in features:
+            p*=(self.weightedprob(f,cat,self.cprob))
+    
+        # Take the natural log and multiply by -2
+        fscore=-2*math.log(p)
+    
+        # Use the inverse chi2 function to get a probability
+        return self.invchi2(fscore,len(features)*2)
+      
+    def invchi2(self,chi, df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1, df//2):
+            term *= m / i
+            sum += term
+        return min(sum, 1.0)   
+    def __init__(self, features):
+        classifier.__init__(self, features)
+        self.minimums = {}
+    
+    def setminimum(self, cat, min):
+        self.minimums[cat] = min
+        
+    def getminimum(self,cat):
+        if cat not in self.minimums: return 0
+        return self.minimums[cat]    
+         
+    def classify(self, item, default=None):
+        best = default
+        max = 0.0
+        for c in self.categories():
+            p=self.fisherprob(item, c)
+            if p>self.getminimum(c) and p>max:
+                max = p
+                best = c
+        return best
--- classifier/naivebayes.py
+++ classifier/naivebayes.py
@@ -2,9 +2,25 @@ import sys, classifier
 sys.path.append('./classifier.py')
 
 
-c1=classifier.naivebayes(classifier.getwords)
+#c1=classifier.naivebayes(classifier.getwords)
+c1 = classifier.fisherclassifier(classifier.getwords)
 
 c1.sampletrain()
+#print(c1.weightedprob('money','bad', c1.cprob))
+
+#print(c1.cprob('quick','good'))
+#print(c1.fisherprob('quick rabbit','good'))
+
+
+print(c1.classify('quick rabbit'))
+
+print(c1.classify('quick money'))
+
+c1.setminimum('bad', 0.8)
+print(c1.classify('quick money'))
+
+c1.setminimum('good', 0.4)
+print(c1.classify('quick money'))
 
 
 #print(c1.fprob('quick','good'))
@@ -12,6 +28,11 @@ c1.sampletrain()
 #print(c1.weightedprob('money','good', c1.fprob))
 #c1.sampletrain()
 #print(c1.weightedprob('money','good', c1.fprob))
+#print(c1.classify('quick money',default='unknow'))
 
-print(c1.prob('quick rabbit','good'))
-print(c1.prob('quick rabbit','bad'))
+#c1.setthreholds('bad', 3.0)
+
+#print(c1.classify('quick money',default='unknow'))
+
+#for i in range(10): c1.sampletrain()
+#print(c1.classify('quick money',default='unknow'))
