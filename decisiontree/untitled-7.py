ed0312c5b32656f9d686c018b8cf4cdc7551e84d
周雪 <zhouxue@quancheng-ec.com>
2017-03-05 20:30:28 +0800

    add decision tree

---
+++ decisiontree/untitled-7.py
@@ -0,0 +1,16 @@
+import sys, treepredict
+sys.path.append('./treepredict.py')
+
+tpredict = treepredict.decisionnode()
+#print(tpredict.divideset(treepredict.my_data, 2, 'yes'))
+
+
+
+
+tree=treepredict.buildtree(treepredict.my_data)
+treepredict.printtree(tree)
+
+#print(treepredict.classify(['(direct)','USA','yes',5], tree))
+
+treepredict.prune(tree,1.0)
+treepredict.printtree(tree)
